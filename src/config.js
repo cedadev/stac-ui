809bcadd189b8fdbfd8a41e75fde8e0305706665
rsmith013 <richard.d.smith@stfc.ac.uk>
2021-08-26 10:04:38 +0100

    reverting to point to master

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@
 //to take effect
 
 //Address of stac API enpoint
-export const REACT_APP_STAC_API = 'https://stac-elasticsearch-summary-switch.130.246.131.9.nip.io/';
+export const REACT_APP_STAC_API = 'https://stac-elasticsearch-master.130.246.131.9.nip.io/';
