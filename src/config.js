0ff1d5d15fe744e214f5547c8fca557d426df42a
rsmith013 <richard.d.smith@stfc.ac.uk>
2021-10-13 10:23:24 +0100

    implmenting build based on environment variables with a backup

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 //to take effect
 
 //Address of stac API enpoint
-export const REACT_APP_STAC_API = 'https://api.stac.ceda.ac.uk/';
+export const REACT_APP_STAC_API=process.env.REACT_APP_STAC_API || 'api.stac.ceda.ac.uk';
 // export const REACT_APP_STAC_API = 'https://stac-elasticsearch-master.130.246.131.9.nip.io/';
 // export const REACT_APP_STAC_API = 'https://stac-elasticsearch-global-queryables.130.246.131.9.nip.io/';
